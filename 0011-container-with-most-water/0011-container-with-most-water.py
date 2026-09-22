8f33dacc3db5f5f20cd6b09a910425e1bef44d52
Saigattem <180010228+Saigattem@users.noreply.github.com>
2026-09-22 18:56:33 +0530

    Time: 47 ms (92%), Space: 29.8 MB (15.17%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -1,14 +1,3 @@
-# class Solution:
-    # def maxArea(self, height: list[int]) -> int:
-    #     max_water=0
-    #     for i in range(len(height)):
-    #         for j in range(i+1,len(height)):
-    #             width=j-i
-    #             h=min(height[i],height[j])
-    #             area=width*h
-    #             if area>max_water:
-    #                 max_water=area
-    #     return max_water
 class Solution:
     def maxArea(self, height):
         left = 0
