c7d15659b80de507da6a1d327ea4f6b86de77a32
Saigattem <180010228+Saigattem@users.noreply.github.com>
2026-09-24 18:27:51 +0530

    Time: 19 ms (16%), Space: 20.2 MB (21.1%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s=sorted(s)  #Before:s = "anagram" After sorting:s = ['a','a','a','g','m','n','r']
+        t=sorted(t)  #Before:t = "nagaram"After sorting:t = ['a','a','a','g','m','n','r']
+        if s==t:  #s==t
+            return True #true
+
+        return False
